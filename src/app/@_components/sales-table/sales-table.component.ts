962fd51da615f8a2bdf2996047c84cdab5613a85
Виталий Шебаниц <mactepwar@outlook.com>
2019-09-14 02:49:53 +0200

    Добавил частичный компонент табилцы продаж, верстку для окна продаж

--- src/app/@_components/sales-table/sales-table.component.ts
+++ src/app/@_components/sales-table/sales-table.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+import { Product } from 'src/app/@_models/Product';
+import { ProductService } from 'src/app/@_core/product/product.service';
 
 @Component({
   selector: 'app-sales-table',
   templateUrl: './sales-table.component.html',
   styleUrls: ['./sales-table.component.scss']
 })
-export class SalesTableComponent implements OnInit {
+export class SalesTableComponent implements OnInit,AfterViewInit {
 
-  constructor() { }
+  @ViewChild('thead',{static:true}) thead_DataTable:ElementRef;
+  @ViewChild('tbody',{static:true}) tbody_DataTable:ElementRef;
+
+  Products:Array<Product>
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit() {
+    this.Products = this.productService.GetTestProducts();
+
+   
+
+  }
+  ngAfterViewInit(){
+     //console.log(this.thead_DataTable.nativeElement);
+
+    //  let s = this.thead_DataTable.nativeElement.querySelectorAll('th');
+    //  let f = this.tbody_DataTable.nativeElement.querySelectorAll('td');
+ 
+    //  console.log(f);
+    //    console.log(s);
+ 
+    //  s.forEach(function(ind,el){
+    //    el.style.width = f[ind].style.width + "px";
+    //  });
+     //console.log(s[0].style.width = "200px");
   }
 
 }
--- src/app/@_core/product/product.service.ts
+++ src/app/@_core/product/product.service.ts
@@ -12,7 +12,7 @@ export class ProductService {
 
   GetTestProducts():Array<Product>{
     for(let i = 0 ; i < 20; i = i + 1){
-      this.Products.push(new Product(111, "Картошка" + i, i, i * 0.6, 0, "кг"));
+      this.Products.push(new Product(111, "Картошка" + i, i, parseInt((i * 0.6).toFixed(2)), 0, "кг"));
     }
     return this.Products;
   }
